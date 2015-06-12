cff9b2609cbdeb16895745fb4f4fce319112d680
trotyl <trotyl@qq.com>
2015-06-12 16:40:59 +0800

    增加题解05.02

---
+++ CodeHunt/05_Conditionals2/05_02.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(int i)
+    {
+        return (i.ToString().Distinct().Count() == 1 ? "" : "not a ") + "fancy year";
+    }
+}
