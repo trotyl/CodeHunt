fcf900f3107929ee54d808443ebe790173597fe3
trotyl <trotyl@qq.com>
2015-06-12 16:25:33 +0800

    增加题解03.07

---
+++ CodeHunt/03_Loops2/03_07.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        return String.Join("", s.Select(x => (char)(117 < x ? x - 21 : x + 5)));
+    }
+}
