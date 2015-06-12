b2ba3140c6006ec9b00f2ca77790cfba282bed43
trotyl <trotyl@qq.com>
2015-06-12 16:38:31 +0800

    增加题解04.11

---
+++ CodeHunt/04_Conditionals/04_11.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(int i, int j, int k)
+    {
+        return (i / (float)j == j / (float)k && (i != j)) ? "yes!" : "no";
+    }
+}
