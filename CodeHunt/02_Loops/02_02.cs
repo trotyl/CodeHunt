fecc946a3aeb9536e6a1ecd020f5c13e1041b101
trotyl <trotyl@qq.com>
2015-06-12 16:09:34 +0800

    增加题解02.02

---
+++ CodeHunt/02_Loops/02_02.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int n)
+    {
+        return Enumerable.Range(0, n).Select(x => n * x).ToArray();
+    }
+}
