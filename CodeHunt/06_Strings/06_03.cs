db6f4dcec1d882b2892690e767db5ba5192f5b71
trotyl <trotyl@qq.com>
2015-06-12 21:38:55 +0800

    增加题解06.03

---
+++ CodeHunt/06_Strings/06_03.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; i < s.Length; i++)
+        {
+            sb.Append(i == s.Length - 1 || s[i + 1] == ' ' ? char.ToUpper(s[i]) : s[i]);
+        }
+        return sb.ToString();
+    }
+}
