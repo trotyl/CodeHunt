73082f2b1a3fda84afdea4dae885f5f345289a86
trotyl <trotyl@qq.com>
2015-06-12 22:27:02 +0800

    增加题解07.09

---
+++ CodeHunt/07_Strings2/07_09.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(int i, string s)
+    {
+        return String.Join(" ", Enumerable.Repeat(s, i));
+    }
+}
