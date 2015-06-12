bb80fdc1ace498fe0d9b651b220e908ce10dd837
trotyl <trotyl@qq.com>
2015-06-12 21:42:06 +0800

    增加题解06.07

---
+++ CodeHunt/06_Strings/06_07.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        return s.Substring(s.Length / 2 + 1).ToUpper() + s.Substring(s.Length / 2);
+    }
+}
