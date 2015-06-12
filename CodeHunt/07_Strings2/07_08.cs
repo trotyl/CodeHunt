104e595f56eaa50d58b26234cdf0db8cd051f05b
trotyl <trotyl@qq.com>
2015-06-12 22:26:14 +0800

    增加题解07.08

---
+++ CodeHunt/07_Strings2/07_08.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+    public static bool Puzzle(string input, string a, string b, string c)
+    {
+        return input.StartsWith(a) && input.Contains(b) && input.EndsWith(c);
+    }
+}
