550c56f584811fb09bb0e0969bbf8c11d9f726f8
trotyl <trotyl@qq.com>
2015-06-12 15:49:07 +0800

    增加题解00.02

---
+++ CodeHunt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeHunt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = global::Program.Puzzle(0);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
