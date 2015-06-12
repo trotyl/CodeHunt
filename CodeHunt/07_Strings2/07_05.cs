36e85c2c7f56b73f3453ab315850ff390bd19b8e
trotyl <trotyl@qq.com>
2015-06-12 21:56:04 +0800

    增加题解07.05

---
+++ CodeHunt/07_Strings2/07_05.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(string a, string b)
+    {
+        var res = "";
+        for (int i = 0; i < a.Length; i++)
+            res += i % 2 == 0 ? b[i] : a[i];
+        return res;
+    }
+}
