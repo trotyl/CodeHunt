46a1b73d641e0dfa81e9de0bc104df3af3f07e0e
trotyl <trotyl@qq.com>
2015-06-12 22:38:35 +0800

    增加题解07.10

---
+++ CodeHunt/07_Strings2/07_10.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(int t)
+    {
+        var raw = string.Join(" ", Enumerable.Repeat("a b c d e f g h i j k l m n o p q r s t u v w x y z", t));
+        return raw.Substring(0, raw.Length - t * 2 + 1) + "z";
+    }
+}
