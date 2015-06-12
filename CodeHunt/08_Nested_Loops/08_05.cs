e9cd41af3968e18302c348b81d9d07555e9517c3
trotyl <trotyl@qq.com>
2015-06-12 23:25:36 +0800

    增加题解08.05

---
+++ CodeHunt/08_Nested_Loops/08_05.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(string a)
+    {
+        return string.Join(" ", Enumerable.Repeat(a, a.Length).Select((x, i) => string.Join("", x.Select((y, j) => i == j ? '_' : x[j]))));
+    }
+}
