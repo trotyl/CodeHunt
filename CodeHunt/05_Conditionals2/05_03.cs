b9b8ef4f0874bcb9265d161711a13a13535d2d5c
trotyl <trotyl@qq.com>
2015-06-12 16:41:41 +0800

    增加题解05.03

---
+++ CodeHunt/05_Conditionals2/05_03.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static bool Puzzle(int a, int b, int c)
+    {
+        return Math.Pow(Math.Min(a, b), 2) == (Math.Max(a, b) + c) * (Math.Abs(c - Math.Max(a, b)));
+    }
+}
