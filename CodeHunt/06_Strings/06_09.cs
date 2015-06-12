0c24856057cfeddd79663f876cec823075b430fd
trotyl <trotyl@qq.com>
2015-06-12 21:43:35 +0800

    增加题解06.09

---
+++ CodeHunt/06_Strings/06_09.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(string a, string b)
+    {
+        int x = a.Length, y = b.Length;
+        return x > y ? a : x < y ? b : a + b;
+    }
+}
