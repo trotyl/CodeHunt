cde54ee7fed340a7077e078179c4fb077cfef918
trotyl <trotyl@qq.com>
2015-06-12 16:24:34 +0800

    增加题解03.06

---
+++ CodeHunt/03_Loops2/03_06.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(string word)
+    {
+        return String.Join(" ", new String('_', word.Length).ToArray());
+    }
+}
