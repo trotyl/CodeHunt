c200511ff8f0805305c5579205fb222a62db9f31
trotyl <trotyl@qq.com>
2015-06-12 16:40:23 +0800

    增加题解05_01

---
+++ CodeHunt/05_Conditionals2/05_01.cs
@@ -0,0 +1,9 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        int l = s.Length;
+        return l < 4 ? "short" : l < 8 ? "average" : l < 15 ? "long" : "super long";
+    }
+}
