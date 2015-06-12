e5309dce8385c82c26bde69a8300b8bc104da22e
trotyl <trotyl@qq.com>
2015-06-12 22:40:13 +0800

    增加题解08.01

---
+++ CodeHunt/08_Nested_Loops/08_01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static int Puzzle(int i, int j)
+    {
+        var res = 0;
+        for (int k = i; k <= j; k++) res += Factorial(k);
+        return res;
+    }
+
+    private static int Factorial(int n)
+    {
+        return n == 1 ? 1 : n * Factorial(n - 1);
+    }
+}
