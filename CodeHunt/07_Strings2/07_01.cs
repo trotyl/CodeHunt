688ecfe8cfaabbd1f8d0622e5448ab1ebd2ad928
trotyl <trotyl@qq.com>
2015-06-12 21:53:21 +0800

    增加题解07.01

---
+++ CodeHunt/07_Strings2/07_01.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(string one, string two, string three)
+    {
+        return two + three + one + one + three + two;
+    }
+}
