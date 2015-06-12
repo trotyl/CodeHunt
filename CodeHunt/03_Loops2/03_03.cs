d70420fe415987299e2dfdff50945475f130b7e7
trotyl <trotyl@qq.com>
2015-06-12 16:22:02 +0800

    增加题解03.03

---
+++ CodeHunt/03_Loops2/03_03.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int Puzzle(int lowerBound, int upperBound)
+    {
+        return Enumerable.Range(lowerBound, upperBound - lowerBound + 1).Aggregate(1, (sum, num) => sum * num);
+    }
+}
