90fd0d10aa027d5f745776b9f56c8664c912ef0e
trotyl <trotyl@qq.com>
2015-06-12 16:45:20 +0800

    增加题解06.02

---
+++ CodeHunt/06_Strings/06_02.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        return String.Join("", s.Select((c, i) => i % 2 == 0 ? Char.ToUpper(c) : c));
+    }
+}
