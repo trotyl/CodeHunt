746fa8dad56759abb7a478635b51aa5bd4f6be1f
trotyl <trotyl@qq.com>
2015-06-13 15:07:57 +0800

    增加题解09.12

---
+++ CodeHunt/09_1D_Arrays/09_12.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] numbers, int amount)
+    {
+        return numbers.Select((c, i) => numbers[(i + numbers.Length - amount) % numbers.Length]).ToArray();
+    }
+}
