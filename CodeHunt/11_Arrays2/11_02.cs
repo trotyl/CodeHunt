dc897667ee2c0a100a864073e726590320f843ea
trotyl <trotyl@qq.com>
2015-06-13 19:23:20 +0800

    增加题解11.02

---
+++ CodeHunt/11_Arrays2/11_02.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string[] Puzzle(string[] a, int amount)
+    {
+        return a.Select(x => Enumerable.Repeat(x, amount)).SelectMany(x => x).ToArray();
+    }
+}
