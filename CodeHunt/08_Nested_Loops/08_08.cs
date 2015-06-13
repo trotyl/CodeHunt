a1486c1c068faba01b12d52733e0999c7d62eeb6
trotyl <trotyl@qq.com>
2015-06-13 14:36:13 +0800

    增加题解09.01

--- CodeHunt/08_Nested_Loops/08_08.cs
+++ CodeHunt/08_Nested_Loops/08_08.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static string Puzzle(int n)
     {
-        return String.Join(" ", Enumerable.Range(1, n).Concat(Enumerable.Range(1, n - 1).Reverse()).Select(x => String.Join("", Enumerable.Repeat(x, x))));
+        return string.Join(" ", Enumerable.Range(1, n).Concat(Enumerable.Range(1, n - 1).Reverse()).Select(x => string.Join("", Enumerable.Repeat(x, x))));
     }
 }
