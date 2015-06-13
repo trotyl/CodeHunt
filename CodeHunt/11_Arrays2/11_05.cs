ea119710f45b76c68448bf3b7b35e93d2859f016
trotyl <trotyl@qq.com>
2015-06-13 19:43:54 +0800

    增加题解11.05

---
+++ CodeHunt/11_Arrays2/11_05.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static string[] Puzzle(string[] list)
+    {
+        var res = new string[(list.Length + 1) / 2];
+        int i = 0;
+        while (i < list.Length)
+        {
+            res[i / 2] = list[i] + (i + 1 < list.Length ? list[i + 1] : "");
+            i += 2;
+        }
+        return res;
+    }
+}
