84ebb8fd529eb7e7e06d0e7703bc0fb1b2f38573
trotyl <trotyl@qq.com>
2015-06-14 02:41:12 +0800

    增加题解12.10

---
+++ CodeHunt/12_Search_Sort/12_10.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static bool Puzzle(string[] words)
+    {
+        for (int i = 0; i < words.Length - 1; i++)
+        {
+            if (string.Compare(words[i], words[i + 1]) > 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
