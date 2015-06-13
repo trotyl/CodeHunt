7d3eb823eb6bc4b860c2ba2035b0638b224b25f0
trotyl <trotyl@qq.com>
2015-06-13 19:32:32 +0800

    增加题解11.03

---
+++ CodeHunt/11_Arrays2/11_03.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+    public static bool Puzzle(int[][] input)
+    {
+        var left = 0;
+        var right = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            left += input[i][i];
+            right += input[i][input.Length - i - 1];
+        }
+        return left == right;
+    }
+}
--- CodeHunt/Program.cs
+++ CodeHunt/Program.cs
@@ -11,13 +11,7 @@ namespace CodeHunt
         static void Main(string[] args)
         {
             var input = Enumerable.Repeat(Enumerable.Range(0, 10).ToArray(), 10).ToArray();
-            var sums = new int[input.Length];
-            var res = input.Select(x => 
-                x.Select((y, j) => {
-                    sums[j] += y;
-                    return y;
-                }));
-            Console.WriteLine(res.ToArray());
+            
 
             Console.ReadKey();
         }
