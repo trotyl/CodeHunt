cdd8e3182676f6f54188e04e825db64dcae8d64f
trotyl <trotyl@qq.com>
2015-06-13 19:20:31 +0800

    增加题解11.01

---
+++ CodeHunt/11_Arrays2/11_01.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] a, int[] b)
+    {
+        return Enumerable.Range(0, Math.Max(a.Length, b.Length)).Select(i => i >= b.Length ? a[i] : Math.Abs((i >= a.Length ? 0 : a[i]) - b[i])).ToArray();
+    }
+}
