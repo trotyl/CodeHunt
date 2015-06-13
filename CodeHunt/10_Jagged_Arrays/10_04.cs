2675d9bf5c0662b7cd1404935333c9f115d61d25
trotyl <trotyl@qq.com>
2015-06-13 16:08:11 +0800

    增加题解10.04

---
+++ CodeHunt/10_Jagged_Arrays/10_04.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[][] Puzzle(int[][] input)
+    {
+        return input.Select(a => a.Select(i => i * 2).ToArray()).ToArray();
+    }
+}
