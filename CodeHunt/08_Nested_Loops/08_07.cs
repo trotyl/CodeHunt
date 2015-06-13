becdda01d99560aa298ec17c3e60d0289434162f
trotyl <trotyl@qq.com>
2015-06-13 14:26:38 +0800

    增加题解08.07

---
+++ CodeHunt/08_Nested_Loops/08_07.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(int h, int w)
+    {
+        return string.Join(" ", Enumerable.Range(0, h).Select(i => string.Join("", Enumerable.Range(0, w).Select(j => (i + j) % 2 == 0 ? 'x' : 'o'))));
+    }
+}
