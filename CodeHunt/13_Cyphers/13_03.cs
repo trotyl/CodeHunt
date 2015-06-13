9b55b6e514646ecad49f61d30e0b918f84e2886a
trotyl <trotyl@qq.com>
2015-06-14 03:27:58 +0800

    增加题解13.03

---
+++ CodeHunt/13_Cyphers/13_03.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(string s)
+    {
+        return new string(s.Select((x, i) => {
+            var tmp = x + 4 * i + 1;
+            return (char)(tmp > 'z' ? (tmp - 'a') % 26 + 'a' : tmp);
+        }).ToArray());
+    }
+}
