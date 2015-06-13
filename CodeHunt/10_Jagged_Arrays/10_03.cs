2a78c53d6faf3c6d0bad4b996313115d7d281129
trotyl <trotyl@qq.com>
2015-06-13 16:04:55 +0800

    增加题解10.03

---
+++ CodeHunt/10_Jagged_Arrays/10_03.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[][] Puzzle(int length, int x)
+    {
+        return Enumerable.Repeat(Enumerable.Repeat(x, length).ToArray(), length).ToArray();
+    }
+}
