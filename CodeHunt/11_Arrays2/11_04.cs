863af4568dc31957c4bef0369a73850294d7f7c8
trotyl <trotyl@qq.com>
2015-06-13 19:34:23 +0800

    增加题解11.04

---
+++ CodeHunt/11_Arrays2/11_04.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] list, int modBy)
+    {
+        return list.Select(x => x % modBy).ToArray();
+    }
+}
