71a8d9f9090f5e2be37e03b1bccab9772a40c93b
trotyl <trotyl@qq.com>
2015-06-13 20:21:01 +0800

    增加题解12.06

---
+++ CodeHunt/12_Search_Sort/12_06.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] numbers, int x, int y)
+    {
+        return numbers.Select(t => t == x ? y : t).ToArray();
+    }
+}
