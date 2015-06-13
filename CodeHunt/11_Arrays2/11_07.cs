1f8e2657ae9b236768e5430e9748c1b236f08fda
trotyl <trotyl@qq.com>
2015-06-13 20:02:28 +0800

    增加题解11.07

---
+++ CodeHunt/11_Arrays2/11_07.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program
+{
+    public static string[][] Puzzle(string[][] input)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                var t = input[i][j];
+                input[i][j] = input[j][i];
+                input[j][i] = t;
+            }
+        }
+        return input;
+    }
+}
