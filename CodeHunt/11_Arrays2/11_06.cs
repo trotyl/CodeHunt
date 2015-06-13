da6d7e30dfa441626e51fd03054d69092f3e1518
trotyl <trotyl@qq.com>
2015-06-13 19:49:23 +0800

    增加题解11.06

---
+++ CodeHunt/11_Arrays2/11_06.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string[] Puzzle(int[] grades, int A, int B, int C, int D)
+    {
+        return grades.Select(x => x >= A ? "A" : x >= B ? "B" : x >= C ? "C" : x >= D ? "D" : "E").ToArray();
+    }
+}
