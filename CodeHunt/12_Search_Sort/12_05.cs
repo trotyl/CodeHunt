7d434369073f084a04273ac39b6e16709e2b5efd
trotyl <trotyl@qq.com>
2015-06-13 20:18:59 +0800

    增加题解12.05

---
+++ CodeHunt/12_Search_Sort/12_05.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] numbers, int x)
+    {
+        return numbers.Select((t, i) => t == x ? i : -1).Where(t => t >= 0).ToArray();
+    }
+}
--- CodeHunt/Program.cs
+++ CodeHunt/Program.cs
@@ -11,6 +11,7 @@ namespace CodeHunt
         static void Main(string[] args)
         {
             var input = Enumerable.Repeat(Enumerable.Range(0, 10).ToArray(), 10).ToArray();
+            var arr = new int[5] { 1, 2, 3, 4, 5 };
             
 
             Console.ReadKey();
