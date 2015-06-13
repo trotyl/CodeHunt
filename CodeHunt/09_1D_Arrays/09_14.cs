ae1eed22b46b4a3229f2e502da409fdba941b46f
trotyl <trotyl@qq.com>
2015-06-13 15:51:47 +0800

    增加题解09.14

---
+++ CodeHunt/09_1D_Arrays/09_14.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static int[] Puzzle(int[] a, int[] b)
+    {
+        return Enumerable.Range(0, Math.Max(a.Length, b.Length)).Select(i => (i < a.Length ? a[i] : 0) + (i < b.Length ? b[i] : 0)).ToArray();
+    }
+}
