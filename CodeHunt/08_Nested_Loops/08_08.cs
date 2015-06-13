2e37c174574115203fb6d601f0369a5941d5d9e0
trotyl <trotyl@qq.com>
2015-06-13 14:29:18 +0800

    增加题解08.08

---
+++ CodeHunt/08_Nested_Loops/08_08.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static string Puzzle(int n)
+    {
+        return String.Join(" ", Enumerable.Range(1, n).Concat(Enumerable.Range(1, n - 1).Reverse()).Select(x => String.Join("", Enumerable.Repeat(x, x))));
+    }
+}
