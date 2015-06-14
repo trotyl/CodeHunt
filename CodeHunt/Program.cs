6a31084f208ccd4bfa6ee41e27a342e58980cf73
trotyl <trotyl@qq.com>
2015-06-14 20:41:50 +0800

    增加题解14.08

--- CodeHunt/Program.cs
+++ CodeHunt/Program.cs
@@ -11,12 +11,38 @@ namespace CodeHunt
         static void Main(string[] args)
         {
             "123".All(x => x != null);
-            var res = Enumerable.Range(0, 10).Select((x, i) => new { Index = i, Value = x }).GroupBy(x => x.Index / 2).Select(x => (int)(x.ToArray().Average(t => t.Value))).ToArray();
+            var res = Puzzle("pa", "pr");
             Console.WriteLine("{0} {1} {2}", (int)('|'), (int)('}'), (int)('~'));
             Console.WriteLine(res);
             Console.ReadKey();
         }
 
-
+        public static bool Puzzle(string a, string b)
+        {
+            var dic = new Dictionary<char, int>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (dic.ContainsKey(a[i]))
+                {
+                    dic[a[i]]++;
+                }
+                else
+                {
+                    dic[a[i]] = 1;
+                }
+            }
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (dic.ContainsKey(a[i]))
+                {
+                    dic[a[i]]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return dic.All(x => x.Value == 0);
+        }
     }
 }
