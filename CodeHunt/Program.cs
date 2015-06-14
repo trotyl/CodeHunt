5e82b8b1dbb254991ba4c678b660e1d08e48da96
trotyl <trotyl@qq.com>
2015-06-14 21:12:30 +0800

    完成Zone：Code Hunt

--- CodeHunt/Program.cs
+++ CodeHunt/Program.cs
@@ -10,39 +10,7 @@ namespace CodeHunt
     {
         static void Main(string[] args)
         {
-            "123".All(x => x != null);
-            var res = Puzzle("pa", "pr");
-            Console.WriteLine("{0} {1} {2}", (int)('|'), (int)('}'), (int)('~'));
-            Console.WriteLine(res);
-            Console.ReadKey();
-        }
-
-        public static bool Puzzle(string a, string b)
-        {
-            var dic = new Dictionary<char, int>();
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (dic.ContainsKey(a[i]))
-                {
-                    dic[a[i]]++;
-                }
-                else
-                {
-                    dic[a[i]] = 1;
-                }
-            }
-            for (int i = 0; i < b.Length; i++)
-            {
-                if (dic.ContainsKey(a[i]))
-                {
-                    dic[a[i]]--;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return dic.All(x => x.Value == 0);
+            
         }
     }
 }
