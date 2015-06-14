883cc0b472c792d391cb041b9ef67cf19688315d
trotyl <trotyl@qq.com>
2015-06-14 17:04:17 +0800

    更正序号

---
+++ CodeHunt/14_Puzzles/14_03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Program
+{
+    public static bool Puzzle(int x, int f)
+    {
+        return f != 1 && x % f == 0 && IsPrime(f);
+    }
+
+    private static bool IsPrime(int f)
+    {
+        for (int i = 2; i <= Math.Sqrt(f); i++)
+        {
+            if (f % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
