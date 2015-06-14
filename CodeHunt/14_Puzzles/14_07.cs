e692ceb689e60e3c61ea7735377bd9093491d0f9
trotyl <trotyl@qq.com>
2015-06-14 20:18:56 +0800

    增加题解14.07

---
+++ CodeHunt/14_Puzzles/14_07.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+public class Program
+{
+    public static bool Puzzle(string s)
+    {
+        return Enumerable.Range(0, s.Length / 2).All(i => s[i] == s[s.Length - i - 1]);
+    }
+}
--- CodeHunt/Program.cs
+++ CodeHunt/Program.cs
@@ -10,7 +10,7 @@ namespace CodeHunt
     {
         static void Main(string[] args)
         {
-
+            "123".All(x => x != null);
             var res = Enumerable.Range(0, 10).Select((x, i) => new { Index = i, Value = x }).GroupBy(x => x.Index / 2).Select(x => (int)(x.ToArray().Average(t => t.Value))).ToArray();
             Console.WriteLine("{0} {1} {2}", (int)('|'), (int)('}'), (int)('~'));
             Console.WriteLine(res);
