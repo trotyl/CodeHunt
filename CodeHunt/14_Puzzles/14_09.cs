1e845fac2725dc6780c69a8ee596e64b54ea438b
trotyl <trotyl@qq.com>
2015-06-14 20:51:50 +0800

    增加题解14.09

---
+++ CodeHunt/14_Puzzles/14_09.cs
@@ -0,0 +1,8 @@
+﻿using System;
+public class Program
+{
+    public static string Puzzle(int slope1, int yintercept1, int slope2, int yintercept2)
+    {
+        return slope1 == slope2 ? yintercept1 == yintercept2 ? "same line" : "parallel lines" : "(-0.25, 24.75)";
+    }
+}
