0cfe75e88ff05774ad718fc8a2d49706ccb7ba12
trotyl <trotyl@qq.com>
2015-06-15 19:34:24 +0800

    增加题解14.08

---
+++ CodeHunt/14_Puzzles/14_08.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+public class Program
+{
+    public static bool Puzzle(string a, string b)
+    {
+        var dic = new int[26];
+        for (int i = 0; i < a.Length; i++)
+        {
+            dic[a[i] - 'a']++;
+        }
+        for (int i = 0; i < b.Length; i++)
+        {
+            dic[b[i] - 'a']--;
+        }
+        return dic.All(x => x == 0);
+    }
+}
